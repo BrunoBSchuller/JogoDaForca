5616bee20c0eb7226147231dd10ddba8da7dba94
BrunoBSchuller <brunschuller@gmail.com>
2025-03-25 19:27:42 -0300

    Implementada estrutura básica do projet

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        
+        // Vesão 1: Estrutura básica e entrado do usuário
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            while (true) 
+            {
+                Console.Clear();
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("-----------------------------------------------");
+
+                Console.WriteLine("Digite uma letra: ");
+                char chute = Console.ReadLine()[0];  // obtém apenas um caractere do que
+
+                Console.WriteLine(chute);
+
+                Console.ReadLine();
+
+            }
+
+
         }
     }
 }
