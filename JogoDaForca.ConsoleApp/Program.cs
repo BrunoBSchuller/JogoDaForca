dec8ff85f4fa959e2afe309fbd440d3b51731847
BrunoBSchuller <brunschuller@gmail.com>
2025-03-25 21:25:02 -0300

    ATT

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -3,27 +3,95 @@
     internal class Program
     {
         
-        // Vesão 1: Estrutura básica e entrado do usuário
+        // Vesão 2: Exibir palavra oculta com traços
 
         static void Main(string[] args)
         {
 
             while (true) 
             {
-                Console.Clear();
-                Console.WriteLine("-----------------------------------------------");
-                Console.WriteLine("Jogo da Forca");
-                Console.WriteLine("-----------------------------------------------");
+                string palavraSecreta = "MELANCIA";
 
-                Console.WriteLine("Digite uma letra: ");
-                char chute = Console.ReadLine()[0];  // obtém apenas um caractere do que
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
 
-                Console.WriteLine(chute);
+                for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+                {
+                    // acessar o array no indice 0
+
+                    letrasEncontradas[caractere] = '_';
+                }
+
+              
+
+                int quantidadeErros = 0;
+                bool jogadorEnforcou = false;   
+                bool jogadorAcertou = false;
+
+                do
+                {
+                    string dicaDaPalavra = string.Join("", letrasEncontradas);
+
+                    Console.Clear();
+                    Console.WriteLine("-----------------------------------------------");
+                    Console.WriteLine("Jogo da Forca");
+                    Console.WriteLine("-----------------------------------------------");
+                    Console.WriteLine("Palavra secreta: " + dicaDaPalavra);
+                    Console.WriteLine("-----------------------------------------------");
+                    Console.WriteLine("Quantidade de erros: " + quantidadeErros);
+                    Console.WriteLine("-----------------------------------------------");
+
+                    char chute = Console.ReadLine()[0];
+
+                    Console.Write("Digite uma letra:");
+                    
+                    bool letraFoiEncontrada = false;
+ 
+                    for (int contador = 0; contador < palavraSecreta.Length; contador++)
+                    {
+                        char letraAtual = palavraSecreta[contador];
+
+                        if (chute == letraAtual)
+                        {
+                            letrasEncontradas[contador] = letraAtual;
+                            letraFoiEncontrada = true;
+                        }
+                       
+                    }
+
+                   
+                    if (letraFoiEncontrada == false)
+                        quantidadeErros++;
+
+
+                    dicaDaPalavra = string.Join("", letrasEncontradas);
+
+                        jogadorAcertou = dicaDaPalavra == palavraSecreta;
+
+                    jogadorEnforcou = quantidadeErros == 5;
+
+                    
+
+                    if (jogadorAcertou)
+                    {
+                        Console.WriteLine("-----------------------------------------------");
+                        Console.WriteLine("Você acertou a palavra secreta!! Era:" + palavraSecreta);
+                        Console.WriteLine("-----------------------------------------------");
+                    }
+                  
+                    else if (jogadorEnforcou)
+                    {
+                        Console.WriteLine("-----------------------------------------------");
+                        Console.WriteLine("Que azar! A palavra secreta!! Era:" + palavraSecreta);
+                        Console.WriteLine("-----------------------------------------------");
+                    }
+
+
+
+                } while (jogadorAcertou == false && jogadorEnforcou == false); // || = OU
 
                 Console.ReadLine();
 
             }
-
 
         }
     }
